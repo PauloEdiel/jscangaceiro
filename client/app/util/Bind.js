2c77999898785096a0c119dec6b1170fb472f48a
PauloEdiel <pauloediel.cavalcante@gmail.com>
2018-10-19 10:17:45 -0300

    Padronização do código com o do Livro!

--- client/app/util/Bind.js
+++ client/app/util/Bind.js
@@ -1,6 +1,7 @@
 class Bind {
-  constructor(model, view, props) {
-    const proxy = ProxyFactory.create(model, ...props, model => {
+  constructor(model, view, ...props) {
+
+    const proxy = ProxyFactory.create(model, props, model => {
       view.update(model)
     });
     return proxy;

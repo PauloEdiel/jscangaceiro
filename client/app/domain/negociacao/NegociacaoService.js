9917d24b911ad5b4bcf175d740a2bd20baebb933
PauloEdiel <pauloediel.cavalcante@gmail.com>
2018-11-05 12:06:05 -0200

    Fim do estudo pelo livro , irei me aprofundar nos conceitos aprendidos até aqui e retornarei para assimilar os novos conteúdos

--- client/app/domain/negociacao/NegociacaoService.js
+++ client/app/domain/negociacao/NegociacaoService.js
@@ -1,27 +1,19 @@
 class NegociacaoService {
 
+  constructor() {
+    this._http = new HttpService();
+  }
   obterNegociacoesDaSemana() {
 
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', 'negociacoes/semana');
+    return this._http.get('negociacoes/semana').then(dados => {
 
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-
-            const negociacoes = JSON.
-            parse(xhr.responseText)
-              .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor));
-            resolve(negociacoes);
-          } else {
-            console.log(xhr.responseText);
-            reject('Não foi possivel obter nas Negociações da semana')
-
-          }
-        }
-      };
-      xhr.send();
-    });
+        const negociacoes = dados.map(objeto => new Negociacao(new Date(objeto.data),
+          objeto.quantidade, objeto.valor));
+        return negociacoes;
+      },
+      err => {
+        throw new Error("Não foi possível obter as negociações ");
+      }
+    );
   }
 }
